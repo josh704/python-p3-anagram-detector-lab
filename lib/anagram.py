ff2d1248392d64ddba9bed53b0e2bb75b5f0c839
josh704 <joshuangetich02@gmail.com>
2024-12-01 20:59:55 +0300

    detector-lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    def match(self, possible_anagram):
+        sorted_word = "".join(sorted(self.word)) 
+        matches = []
+        for possible in possible_anagram:
+            if sorted_word =="".join(sorted(possible)):
+             matches.append(possible)
+
+        return matches
